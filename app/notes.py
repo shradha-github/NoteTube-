fe188c3c56e67e578cf4fba151ac8bc50300011d
shradha-github <shraddhalande06@gmail.com>
2026-04-04 12:35:09 +0530

    Updated requirements and fastapi changes

--- app/notes.py
+++ app/notes.py
@@ -2,39 +2,35 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from datetime import datetime
 
-app=FastAPI()
+app = FastAPI()
 
+# Model
 class Notes(BaseModel):
-    title:str
-    youtube_url:str
-    notes:str
+    title: str
+    youtube_url: str
+    notes: str
 
-all_notes=[]    
+# Temporary database
+all_notes = []
+
+# Home route
 @app.get("/")
 def home():
-    return {"Welcome to Home page"}
+    return {"message": "Welcome to Home page"}
 
+# Save notes
 @app.post("/save")
-def save(note:Notes):
-    data={"title":note.title,
-    "youtube_url":note.youtube_url,
-    "notes":note.notes,
-    "date":datetime.now()
+def save(note: Notes):
+    data = {
+        "title": note.title,
+        "youtube_url": note.youtube_url,
+        "notes": note.notes,
+        "date": str(datetime.now())
     }
     all_notes.append(data)
-    return {"Message":"Notes saved","data":data}
+    return {"message": "Notes saved", "data": data}
 
+# Get all notes
 @app.get("/get")
-def get():
+def get_notes():
     return all_notes
-
-
-
-
-
-
-
-
-
-
-
