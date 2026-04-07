944fe4030adb43fe87b3e8f58ba30fd563fa0bab
shradha-github <shraddhalande06@gmail.com>
2026-04-07 21:43:47 +0530

    Fixed CORS issue

--- Backend/main.py
+++ Backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 app.include_router(router)
 
 app.add_middleware(
-    CORSMiddleware,allow_origin=["*"],
+    CORSMiddleware,allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
